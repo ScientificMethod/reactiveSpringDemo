6f1cd46e6418db57a442777b0f949707bd2eca25
Alex Banker <alex@Alexs-MBP.localdomain>
2023-10-09 15:52:58 -0400

    Adjusting gradle and project settings

---
+++ src/integration/java/com/example/javadynamo/JavaDynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javadynamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDynamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
